8dca9a15d5957e5ebff95c5c5bc4681e21b91e3c
mudithakandewatta <mudisankalpa123@gmail.com>
2026-03-17 14:29:13 +0530

    initialized frontend

---
+++ api-gateway/server.js
@@ -0,0 +1,47 @@
+// server.js
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+
+// Patient Service
+app.use("/api/patients", createProxyMiddleware({
+  target: "http://localhost:5001",
+  changeOrigin: true,
+}));
+
+// Doctor Service
+app.use("/api/doctors", createProxyMiddleware({
+  target: "http://localhost:5002",
+  changeOrigin: true,
+}));
+
+// Appointment Service
+app.use("/api/appointments", createProxyMiddleware({
+  target: "http://localhost:5003",
+  changeOrigin: true,
+}));
+
+// Telemedicine Service
+app.use("/api/telemedicine", createProxyMiddleware({
+  target: "http://localhost:5004",
+  changeOrigin: true,
+}));
+
+// Payment Service
+app.use("/api/payment", createProxyMiddleware({
+  target: "http://localhost:5005",
+  changeOrigin: true,
+}));
+
+// Default Gateway Route
+app.get('/', (req, res) => {
+    res.send('API Gateway is running. Routing traffic to microservices...');
+});
+
+app.listen(5000, () => {
+  console.log(`🚦 API Gateway is running on http://localhost:${PORT}`);
+});
